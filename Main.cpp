3fd1dd1f0c438c58e09a68db6e2bf8a87fa8c00e
Kuba Sawicki <kuba_sawicki@onet.pl>
2024-06-03 14:32:51 +0200

    added gui

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,19 @@
+/*******************************************************************************************
+*
+*   raylib [core] example - Picking in 3d mode
+*
+*   Example originally created with raylib 1.3, last time updated with raylib 4.0
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2015-2024 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
 #include "raylib.h"
+
+#include "GUI.h"
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -7,29 +22,99 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);    // Window configuration flags
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    int screenWidth = 1500;
+    int screenHeight = 800;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    InitWindow(screenWidth, screenHeight, "Window title");
+
+    // Define the camera to look into our 3d world
+    Camera camera = { 0 };
+    camera.position = { 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = { 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = { 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+
+    Vector3 cubePosition = { 0.0f, 1.0f, 0.0f };
+    Vector3 cubeSize = { 2.0f, 2.0f, 2.0f };
+
+    Ray ray = { 0 };                    // Picking line ray
+    RayCollision collision = { 0 };     // Ray collision hit info
+
+    // Set up 2D GUI
+    GUI GUI(screenWidth, screenHeight);
+
+    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())        // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+        if (IsCursorHidden()) UpdateCamera(&camera, CAMERA_FIRST_PERSON);
+
+        // Toggle camera controls
+        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+        {
+            if (IsCursorHidden()) EnableCursor();
+            else DisableCursor();
+        }
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            if (!collision.hit)
+            {
+                ray = GetMouseRay(GetMousePosition(), camera);
+
+                // Check collision between ray and box
+                collision = GetRayCollisionBox(ray,{
+                    {
+                        cubePosition.x - cubeSize.x / 2, cubePosition.y - cubeSize.y / 2, cubePosition.z - cubeSize.z / 2
+                    },
+                    {
+                        cubePosition.x + cubeSize.x / 2, cubePosition.y + cubeSize.y / 2, cubePosition.z + cubeSize.z / 2
+                    }
+                });
+            }
+            else collision.hit = false;
+
+            GUI.CheckIfButtonPressed();
+        }
         //----------------------------------------------------------------------------------
 
-        // Draw
+        // Draw 3D
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        BeginMode3D(camera);
+
+        if (collision.hit)
+        {
+            DrawCube(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, RED);
+            DrawCubeWires(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, MAROON);
+
+            DrawCubeWires(cubePosition, cubeSize.x + 0.2f, cubeSize.y + 0.2f, cubeSize.z + 0.2f, GREEN);
+        }
+        else
+        {
+            DrawCube(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, GRAY);
+            DrawCubeWires(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, DARKGRAY);
+        }
+
+        DrawRay(ray, MAROON);
+        DrawGrid(10, 1.0f);
+
+        EndMode3D();
+
+        // Draw 2D
+        //----------------------------------------------------------------------------------
+
+        GUI.DrawGUI(screenWidth, screenHeight);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
